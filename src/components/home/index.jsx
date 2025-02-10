f4f413452f7ddd9ae8194c3fa045063967e7c832
prosenjitsikdar <prosenjitsikdar5@gmail.com>
2025-02-10 15:12:55 +0530

    file

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useAuth } from '../../contexts/authContext';
 import { useNavigate } from 'react-router-dom';
 
